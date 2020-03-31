b58954a2be96c724ebf48f19b0cf8a5a5322b378
prakash <prakashgehlot25698@gmail.com>
2020-03-31 18:55:24 +0530

    Update Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,12 @@ require(`dotenv`).config({
 })
 
 module.exports = {
+	pathPrefix: "/portfolio",
   siteMetadata: {
     // You can overwrite values here that are used for the SEO component
     // Of course you can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/master/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
+    siteTitleAlt: `Alint Antony`,
   },
   plugins: [
     {
@@ -24,9 +25,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Cara - @lekoarts/gatsby-theme-cara`,
-        short_name: `Cara`,
-        description: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+        name: `Alint Antony`,
+        short_name: `Alint`,
+        description: `portfolio site`,
         start_url: `/`,
         background_color: `#141821`,
         theme_color: `#f6ad55`,
